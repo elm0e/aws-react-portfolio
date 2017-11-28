eb1fb95c90de8c521916deca71930987f0e5eed7
Eric Sorensen <eric@ambientsoftwareservices.com>
2017-11-28 16:09:08 -0600

    Update python lambda script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,20 +6,29 @@ import mimetypes
 
 def lambda_handler(event, context):
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')    
+    topic = sns.Topic('arn:aws:sns:us-east-1:758566139275:PortfolioDeployed')
 
-    portfolio_bucket = s3.Bucket('portfolio.ambient.services')
-    build_bucket = s3.Bucket('portfoliobuild.ambient.services')
-
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-            
-    print "Site Deployed"
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        portfolio_bucket = s3.Bucket('portfolio.ambient.services')
+        build_bucket = s3.Bucket('portfoliobuild.ambient.services')
+    
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        print "Site Deployed"
+        topic.publish(Subject='Portfolio Deployment', Message='Portfolio deployed successfully')
+    
+    except:
+        print "Deployment Failed"
+        topic.publish(Subject='Portfolio Deployment', Message='Portfolio deployment failed')
+        raise
     
     return 'Site Deployed'
